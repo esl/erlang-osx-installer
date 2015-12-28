f9f42e51332657b48db1537b3b975f162ee21d3e
Juan Facorro <juan.facorro@gmail.com>
2015-12-28 14:51:08 -0300

    [#2] Create Cocoa project

---
+++ ErlangInstaller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ErlangInstaller
+//
+//  Created by Juan Facorro on 12/28/15.
+//  Copyright © 2015 Erlang Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
