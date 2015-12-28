ec72e55dcd6fde92b7e1f3b10fdd40a0926bd307
Juan Facorro <juan.facorro@gmail.com>
2015-12-28 17:51:14 -0300

    [#5] Run app as agent

--- ErlangInstaller/AppDelegate.swift
+++ ErlangInstaller/AppDelegate.swift
@@ -11,14 +11,32 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var statusItem : NSStatusItem?
 
-
+    override init() {
+        self.statusItem = nil
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        addStatusItem()
+    }
+    
+    func itemClicked(sender : AnyObject) {
+        let alert = NSAlert()
+        alert.messageText = "Hello"
+        alert.runModal()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func addStatusItem() {
+        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+        self.statusItem?.image = NSImage(named: "menu-bar-icon.png")
+        self.statusItem?.button!.target = self
+        self.statusItem?.button!.action = "itemClicked:"
+
     }
 
 
