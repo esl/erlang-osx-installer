d172b7e25995e660cdc1b5250532ceb873f566fe
Juan Facorro <juan.facorro@gmail.com>
2015-12-29 15:34:58 -0300

    [#4] Pred pane. User defined build setting.

---
+++ ErlangInstallerPreferences/ErlangInstallerPreferences.h
@@ -0,0 +1,15 @@
+//
+//  ErlangInstallerPreferences.h
+//  ErlangInstallerPreferences
+//
+//  Created by Juan Facorro on 12/29/15.
+//  Copyright © 2015 Erlang Solutions. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface ErlangInstallerPreferences : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ ErlangInstallerPreferences/ErlangInstallerPreferences.m
@@ -0,0 +1,17 @@
+//
+//  ErlangInstallerPreferences.m
+//  ErlangInstallerPreferences
+//
+//  Created by Juan Facorro on 12/29/15.
+//  Copyright © 2015 Erlang Solutions. All rights reserved.
+//
+
+#import "ErlangInstallerPreferences.h"
+
+@implementation ErlangInstallerPreferences
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
