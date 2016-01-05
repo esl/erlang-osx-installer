91277682628bacd2380d7ef9f5d02b1896036296
Juan Facorro <juan.facorro@gmail.com>
2016-01-05 16:38:56 -0300

    [#10] Load releases to table view

--- ErlangInstallerPreferences/ErlangInstallerPreferences.swift
+++ ErlangInstallerPreferences/ErlangInstallerPreferences.swift
@@ -8,16 +8,39 @@
 
 import PreferencePanes
 
-class ErlangInstallerPreferences: NSPreferencePane {
+class ErlangInstallerPreferences: NSPreferencePane, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet var _window: NSWindow!
     
     @IBOutlet weak var localMainView: NSView!
+    
+    @IBOutlet weak var tableView: NSTableView!
     
     override func assignMainView() {
         self.mainView = self.localMainView
     }
     
     override func mainViewDidLoad() {
+        self.tableView.reloadData()
+        self.tableView.setDelegate(self)
+        self.tableView.setDataSource(self)
+    }
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return ReleaseManager.available.count
+    }
+    
+    func tableView(aTableView: NSTableView, objectValueForTableColumn aTableColumn: NSTableColumn?,row rowIndex: Int) -> AnyObject? {
+        if aTableColumn != nil {
+            return ReleaseManager.available[rowIndex].name
+        } else {
+            return nil
+        }
+    }
+    
+    func alert(msg: String) {
+        let alert = NSAlert()
+        alert.messageText = msg
+        alert.runModal()
     }
 }
