21a62fba8cf8c5bf1442d43ba834a6a60546bdab
Juan Facorro <juan.facorro@gmail.com>
2016-01-06 18:35:41 -0300

    [#10] Changed messages

--- ErlangInstallerPreferences/InstallationPanel.swift
+++ ErlangInstallerPreferences/InstallationPanel.swift
@@ -12,19 +12,20 @@ class InstallationPanel: NSPanel, InstallationProgress {
     @IBOutlet weak var progressBar: NSProgressIndicator!
     @IBOutlet weak var releaseName: NSTextField!
     @IBOutlet weak var informationMessage: NSTextField!
-    
+
     func start(releaseName: String) {
-        self.releaseName.stringValue = releaseName
+        self.releaseName.stringValue = "Release " + releaseName
+        self.informationMessage.stringValue = ""
         self.makeKeyAndOrderFront(self)
     }
-    
+
     func downloading(maxValue: Double) {
+        self.informationMessage.stringValue = "Downloading..."
         self.progressBar.minValue = 0
         self.progressBar.maxValue = maxValue
     }
-    
+
     func download(progress delta: Double) {
         self.progressBar.incrementBy(delta)
     }
-
 }
