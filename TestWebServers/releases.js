a181681e47771b364179a0ff064a94af597a443f
Juan Facorro <juan.facorro@gmail.com>
2016-02-24 14:12:29 -0300

    [#25] Added script files to start test web servers. Call checkForUpdates: when clicking menu item.

---
+++ TestWebServers/releases.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var count = 0;
+
+function handle (req, res) {
+    var index = fs.readFileSync('releases.json');
+    res.writeHead(200, {'Content-Type': 'text/plain', 'content-length' : index.length});
+    res.end(index);
+}
+
+http.createServer(handle).listen(9090);
---
+++ TestWebServers/updates.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var count = 0;
+
+function handle(req, res) {
+    var index = fs.readFileSync('updates.xml');
+    res.writeHead(200, {'Content-Type': 'text/xml', 'content-length' : index.length});
+    res.end(index);
+}
+
+http.createServer(handle).listen(9091);
