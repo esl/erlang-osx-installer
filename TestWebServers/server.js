add3228f9d439d1103d1ad0a849f391d589430c9
Juan Facorro <juan.facorro@gmail.com>
2016-02-24 15:15:33 -0300

    [#25] Serve .dmg files from the test server as well

--- TestWebServers/server.js
+++ TestWebServers/server.js
@@ -4,21 +4,38 @@ var fs = require('fs');
 var count = 0;
 
 function handle(req, res) {
-    var content = "";
+    var content     = "text/plain";
     var contentType = "";
+    var binary      = false;
+    var statusCode  = 200;
+
     switch(req.url) {
         case "/releases":
             content = fs.readFileSync(__dirname + '/releases.json');
-            contentType = "text/plain";
             break;
         case "/updates":
             content = fs.readFileSync(__dirname + '/updates.xml');
             contentType = "text/xml";
             break;
+        default:
+            try {
+                content = fs.readFileSync(__dirname + req.url);
+                contentType = "application/octet-stream";
+                binary = true;
+            } catch (e) {
+                content = "Not Found";
+                statusCode = 404;
+            }
+            break;
+    }
+
+    res.writeHead(statusCode, {'Content-Type': contentType, 'content-length' : content.length});
+    if(binary) {
+        res.end(content, 'binary');
+    } else {
+        res.end(content);
     }
     
-    res.writeHead(200, {'Content-Type': contentType, 'content-length' : content.length});
-    res.end(content);
 }
 
 http.createServer(handle).listen(9090);
